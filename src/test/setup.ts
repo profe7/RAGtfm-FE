647fc941075b1d814695f917b806af5d07f69ce4
profe7 <azka.arafats@gmail.com>
2026-06-19 08:10:46 +0700

    fix: ci

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -2,7 +2,6 @@ import '@testing-library/jest-dom/vitest'
 import { afterEach } from 'vitest'
 import { cleanup } from '@testing-library/react'
 
-// Unmount React trees and reset DOM state between tests.
 afterEach(() => {
   cleanup()
 })
